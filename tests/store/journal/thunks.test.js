495ef58361054ef5d8e4f4fe2b6f55ec4c10b857
Daniel Flores Rodríguez <daniflores374@gmail.com>
2023-07-24 00:23:35 -0600

    hotfixes

--- tests/store/journal/thunks.test.js
+++ tests/store/journal/thunks.test.js
@@ -1,28 +1,35 @@
+// Import required functions from Firebase and Redux files
 import { collection, deleteDoc, getDocs } from "firebase/firestore/lite";
 import { addNewEmptyNote, savingNewNote, setActiveNote } from "../../../src/store/journal/journalSlice";
 import { startNewNote } from "../../../src/store/journal/thunks";
 import { FirebaseDB } from "../../../src/firebase/config";
 
-
+// Test suite for 'journalThunks'
 describe('Pruebas en journalThunks', () => {
    
+    // Define mock functions to simulate Redux store behavior
     const dispatch = jest.fn();
     const getState = jest.fn();
 
+    // Reset mock functions before each test case
     beforeEach( () => jest.clearAllMocks() );
 
-
+    // Test case: 'startNewNote' should create a new empty note
     test('startNewNote debe crear una nueva nota en blanco', async() => {
 
+        // Define a user ID for the test
         const uid = 'TEST-UID';
 
+        // Mock the Redux store to return the defined user ID when 'getState' is called
         getState.mockReturnValue({ auth: { uid:uid } });
 
+        // Call the 'startNewNote' thunk function with the mock store and user ID
         await startNewNote()( dispatch, getState );
         
+        // Expect that the 'savingNewNote' action is dispatched
         expect( dispatch ).toHaveBeenCalledWith( savingNewNote() );
 
-
+        // Expect that the 'addNewEmptyNote' action is dispatched with the expected payload
         expect( dispatch ).toHaveBeenCalledWith( addNewEmptyNote({
             body:'',
             title:'',
@@ -31,6 +38,7 @@ describe('Pruebas en journalThunks', () => {
             imageUrls: [],
         }) );
 
+        // Expect that the 'setActiveNote' action is dispatched with the expected payload
         expect( dispatch ).toHaveBeenCalledWith( setActiveNote({
             body:'',
             title:'',
@@ -39,21 +47,21 @@ describe('Pruebas en journalThunks', () => {
             imageUrls: [],
         }) );
 
-
-        //Delete in Firebase
+        // Delete notes from Firebase
+        // Get a reference to the 'notes' collection for the given user
         const collectionRef = collection(FirebaseDB, `${uid}/journal/notes`);
+
+        // Fetch all documents (notes) from the collection
         const docs = await getDocs( collectionRef );
 
+        // Prepare an array to store delete promises for each note
         const deletePromises = [];
 
+        // Iterate through all documents (notes) and create a delete promise for each
         docs.forEach( doc => deletePromises.push( deleteDoc( doc.ref ) ) );
         
+        // Wait for all delete promises to resolve (all notes are deleted)
         await Promise.all( deletePromises );
 
-    });
-
-    
-    
-
+    },30000);
 });
- 
